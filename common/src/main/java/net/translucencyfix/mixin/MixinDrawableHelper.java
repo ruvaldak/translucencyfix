1af36f2132670f0450b214186123474a29727e19
Ruvaldak <truvaldak@gmail.com>
2023-06-07 13:02:31 -0400

    house cleaning, fix mixin refmap warn, fix forge

--- common/src/main/java/net/translucencyfix/mixin/MixinDrawableHelper.java
+++ common/src/main/java/net/translucencyfix/mixin/MixinDrawableHelper.java
@@ -13,7 +13,5 @@ public class MixinDrawableHelper {
     @Inject(method = "drawTexturedQuad", at = @At("HEAD"))
     private static void drawTexturedQuad(Matrix4f matrices, int x0, int x1, int y0, int y1, int z, float u0, float u1, float v0, float v1, CallbackInfo ci) {
         RenderSystem.enableBlend();
-        //RenderSystem.defaultBlendFunc();
     }
 }
- 
---
+++ forge/src/main/java/net/translucencyfix/forge/translucencyfixForge.java
@@ -0,0 +1,9 @@
+package net.translucencyfix.forge;
+
+import net.translucencyfix.translucencyfix;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(translucencyfix.MOD_ID)
+public class translucencyfixForge {
+    public translucencyfixForge() {}
+}
