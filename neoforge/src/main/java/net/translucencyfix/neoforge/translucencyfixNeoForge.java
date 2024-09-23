de6507d98f2b413f09cf5f3366eec1d1869007f4
VinnyStalck <vinny.polsom@outlook.com>
2024-09-22 23:27:53 -0300

    Created NeoForge version - added yarn compat patch

---
+++ neoforge/src/main/java/net/translucencyfix/neoforge/translucencyfixNeoForge.java
@@ -0,0 +1,9 @@
+package net.translucencyfix.neoforge;
+
+import net.translucencyfix.translucencyfix;
+import net.neoforged.fml.common.Mod;
+
+@Mod(translucencyfix.MOD_ID)
+public class translucencyfixNeoForge {
+    public translucencyfixNeoForge() {}
+}
